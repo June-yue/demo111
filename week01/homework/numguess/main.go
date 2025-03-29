04ea88404345f0a7bc315d14e50a705bb23c4d44
liuyue <2039114083@qq.com>
2025-03-29 21:11:33 +0800

    Add week01\homework\numguess\main.go file

--- week01/homework/numguess/main.go
+++ week01/homework/numguess/main.go
@@ -11,9 +11,17 @@ import (
 var count int = 0
 
 func rule() {
-	fmt.Printf("欢迎来到猜字数游戏！\n规则:\n1.计算机将在1到100之间随机选择一个数字。\n2.你可以选择难度级别（简单、中等、困难)，不同难度对应不同的猜测机会。\n3.请输入您的猜测。\n")
+	fmt.Println("欢迎来到猜字数游戏!")
+	fmt.Println("规则:")
+	fmt.Println("1.计算机将在1到100之间随机选择一个数字。")
+	fmt.Println("2.你可以选择难度级别（简单、中等、困难)，不同难度对应不同的猜测机会。")
+	fmt.Println("3.请输入您的猜测。")
 	fmt.Println()
-	fmt.Printf("请选择难度级别（简单/中等/困难):\n1.简单(10次机会)\n2.中等(5次机会)\n3.困难(3次机会)\n")
+	fmt.Println("请选择难度级别（简单/中等/困难):")
+	fmt.Println("1.简单(10次机会)")
+	fmt.Println("2.中等(5次机会)")
+	fmt.Println("3.困难(3次机会)")
+	fmt.Println()
 }
 
 func random() int {
@@ -25,6 +33,7 @@ func random() int {
 
 func guess(target, number int, start time.Time) {
 	var num int
+	fmt.Println()
 	for i := 1; i <= number; i++ {
 		fmt.Printf("第%d次猜测,请输入您的数字(1-100):", i)
 		fmt.Scanln(&num)
@@ -36,12 +45,11 @@ func guess(target, number int, start time.Time) {
 			fmt.Printf("恭喜您猜对了！您在第%d次猜测中成功。\n", i)
 			now := time.Now()
 			layout := "2006-01-02 15:04:05"
-			fmt.Println(now.Format(layout))
 			writegame("游戏结束时间：" + now.Format(layout))
 			duration := now.Sub(start)
-			fmt.Println(duration)
 			writegame("所用时间：" + duration.String())
 			writegame("游戏结果：成功")
+			writegame(" ")
 			again()
 			return
 		}
@@ -49,12 +57,11 @@ func guess(target, number int, start time.Time) {
 	}
 	now := time.Now()
 	layout := "2006-01-02 15:04:05"
-	fmt.Println(now.Format(layout))
 	writegame("游戏结束时间：" + now.Format(layout))
 	duration := now.Sub(start)
-	fmt.Println(duration)
 	writegame("所用时间：" + duration.String())
 	writegame("游戏结果：失败")
+	writegame(" ")
 	fmt.Println("很遗憾，机会已经用完了。\n")
 	again()
 }
@@ -75,17 +82,15 @@ func again() {
 func game() {
 	target := random()
 	now := time.Now()
-	fmt.Println(target)
 	writegame("随机数为：" + strconv.Itoa(target))
 	layout := "2006-01-02 15:04:05"
-	fmt.Println(now.Format(layout))
 	var num int
 	count++
 	writegame("游戏次数：" + strconv.Itoa(count))
 	writegame("游戏开始时间：" + now.Format(layout))
 	fmt.Printf("输入选择：")
 	fmt.Scanln(&num)
-	fmt.Printf("开始游戏：\n")
+	fmt.Printf("开始游戏：")
 	fmt.Println()
 	switch num {
 	case 1:
